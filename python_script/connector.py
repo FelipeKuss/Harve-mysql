f0ccb5a2252380742c96cd736a2a482ea042d066
Felipe <lufekuss@gmail.com>
2024-01-17 22:24:32 -0300

    5

--- python_script/connector.py
+++ python_script/connector.py
@@ -12,28 +12,35 @@ print("Este é um jogo de pedra, papel ou tesoura")
 print("Por favor insira seu nome:")
 nome_jogador = input()
 
-print("Insira a sua jogada (PEDRA,PAPEL,TESOURA):")
-jogador = input()
-computador = "Pedra"
-if jogador == computador:
-    resultado = 'Empate'
-elif jogador == 'PAPEL':
-    resultado = 'VITÓRIA'
-else:
-    resultado = 'Derrota'
-    
-print(resultado)
-    
-cursor = conn.cursor()
+while True:
+    print("Insira a sua jogada (PEDRA,PAPEL,TESOURA):")
+    jogador = input()
+    # IMPORT RANDOM
+    # COMPUTADOR = RANDOM.CHOICE(["PEDRA","PAPEL","TESOURA"])
+    computador = "Pedra"
+    if jogador == computador:
+        resultado = 'Empate'
+    elif jogador == 'PAPEL':
+        resultado = 'VITÓRIA'
+    else:
+        resultado = 'Derrota'
+        
+    print(resultado)
+        
+    cursor = conn.cursor()
 
-query = f"""
-    INSERT INTO contagem_jogos
-        (nome_player,jogada_player,jogada_computador,resultado)
-    VALUES
-        ('{nome_jogador}','{jogador}','{computador}','{resultado[0]}')
-"""
+    query = f"""
+        INSERT INTO contagem_jogos
+            (nome_player,jogada_player,jogada_computador,resultado)
+        VALUES
+            ('{nome_jogador}','{jogador}','{computador}','{resultado[0]}')
+    """
 
 
-cursor.execute(query)
-conn.commit()
-cursor.close()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    print("Deseja continuar jogando?(Y/N)")
+    continuar = input()
+    if continuar =='N':
+        break
