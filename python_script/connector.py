eead022b1b7f5cc46bae9e3ccfe09e7df53a5bbe
Felipe <lufekuss@gmail.com>
2024-01-17 21:27:31 -0300

    comit 4

--- python_script/connector.py
+++ python_script/connector.py
@@ -7,6 +7,22 @@ conn = mysql.connector.connect (
     database='meu',
     port='3307'
 )
+
+CREATE TABLE meu.contagem_jogos (
+nome_player 		VARCHAR(20),
+jogada_player 		VARCHAR(10),
+jogada_computador 	VARCHAR(10),
+flag_vitoria		VARCHAR(1),
+data_registro		DATETIME DEFAULT CURRENT_TIMESTAMP()
+
+);
+
+
+INSERT INTO contagem_jogos (nome_player,jogada_player,jogada_computador,flag_vitoria)
+VALUES ('Jogador','Pedra','Pedra','E')
+;
+
+
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM contagem_jogos")
 row = cursor.fetchall()
